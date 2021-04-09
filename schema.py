61a19b03b02e6d3550faff64cc1e500a0fd9a795
victorhugo <victorblog410@gmail.com>
2021-04-09 16:00:57 -0300

    create tables

--- schema.py
+++ schema.py
@@ -4,19 +4,29 @@ conn = sqlite3.connect('olist_store.db')
 
 cursor = conn.cursor()
 
-cursor.execute(
-CREATE TABLE category(
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR NOT NULL,
-        description TEXT NOT NULL
-                    ); 
-CREATE TABLE product(
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR NOT NULL,
-        description TEXT NOT NULL,
-        value FLOAT NOT NULL,
-        FORENKEY (name, name) REFERENCE (category) (name, name)
-                    );
-                )
-print('Table create with sucess')
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS category(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL
+        ); 
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS  product(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    value FLOAT NOT NULL,
+    FOREIGN KEY (name) REFERENCES category (name)
+    );
+
+
+
+
+print('Tabela criada com sucesso.')
+# desconectando...
 conn.close()
+
+
+

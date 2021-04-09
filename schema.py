40e792058e5e3f0c6c9ec7fd4913feded510c5ef
victorhugo <victorblog410@gmail.com>
2021-04-09 16:20:45 -0300

    REFERENCES category

--- schema.py
+++ schema.py
@@ -5,22 +5,26 @@ conn = sqlite3.connect('olist_store.db')
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS  product(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR NOT NULL,
-    description TEXT NOT NULL,
-    value FLOAT NOT NULL
-    );
-""")
-
-
-cursor.execute("""
 CREATE TABLE IF NOT EXISTS category(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL,
     description TEXT NOT NULL
         ); 
 """)
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS  product(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    value FLOAT NOT NULL,
+    FOREIGN KEY (name) REFERENCES category (name)
+    );
+""")
+
+
+
 
 print('Tabela criada com sucesso.')
 conn.close()
